9bb34c14466e721c62ed237e8a4fea4dff844805
hono3bono3 <97086033+hono3bono3@users.noreply.github.com>
2022-05-06 19:07:38 +0900

    LineChart更新

--- src/components/LineChart/LineChart.stories.tsx
+++ src/components/LineChart/LineChart.stories.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { LineChart } from "./index";
+import { LineChart, LineChartProps } from "./index";
 
 export default {
   title: "LineChart",
   component: LineChart,
 };
+const props: LineChartProps = {
+  series: [
+    { type: "line", name: "Line1", data: [1, 2, 3, 4, 5] },
+    { type: "line", name: "Line2", data: [2, 4, 6, 8, 10] },
+    { type: "line", name: "Line3", data: [3, 6, 9, 12, 15] },
+  ],
+};
 
 export const Basic = () => (
   <div>
-    <LineChart />
+    <LineChart {...props} />
   </div>
 );
--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
-const defaultOptions = {
+const defaultOptions: Highcharts.Options = {
   title: {
-    text: "人口増減率",
+    text: "",
   },
   plotOptions: {
     series: {
@@ -30,35 +30,18 @@ const defaultOptions = {
     align: "right",
     verticalAlign: "middle",
   },
-  series: [
-    {
-      name: "兵庫県",
-      data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
-    },
-    {
-      name: "北海道",
-      data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
-    },
-    {
-      name: "東京都",
-      data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
-    },
-    {
-      name: "静岡県",
-      data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
-    },
-    {
-      name: "群馬県",
-      data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
-    },
-  ],
+  series: [],
 };
 
-export const LineChart: any = () => {
-  const options = { ...defaultOptions };
+export type LineChartProps = {
+  series: Highcharts.SeriesLineOptions[];
+};
+
+export const LineChart = ({ series }: LineChartProps) => {
+  const _options = { ...defaultOptions, ...{ series } };
   return (
     <div>
-      <HighchartsReact highcharts={Highcharts} options={options} />
+      <HighchartsReact highcharts={Highcharts} options={_options} />
     </div>
   );
 };
